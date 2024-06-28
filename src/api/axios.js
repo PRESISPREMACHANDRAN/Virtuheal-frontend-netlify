bb271d13896464e8255eb6975cbcbafe8b0e2cec
PRESISPREMACHANDRAN <presipremachandran@gmail.com>
2024-06-28 14:36:29 +0530

    Updated BASE_URL in axios.js for PythonAnywhere backend

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 // const BASE_URL = 'http://35.232.150.232/api';
-const BASE_URL = 'http://localhost:8000/api';
+// const BASE_URL = 'http://localhost:8000/api';
+const BASE_URL = 'https://presi.pythonanywhere.com/api';
 
 export default axios.create({
     baseURL: BASE_URL,
